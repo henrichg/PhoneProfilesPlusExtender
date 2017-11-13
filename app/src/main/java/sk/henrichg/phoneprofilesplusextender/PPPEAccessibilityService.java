dfafe9b7150a70d5a63e887892c0c6761401b404
henrichg <henrich.gron@chello.sk>
2017-11-13 16:23:40 +0100

    Initial commit

---
+++ app/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEAccessibilityService.java
@@ -0,0 +1,97 @@
+package sk.henrichg.phoneprofilesplusextender;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
+
+import java.util.List;
+
+public class PPPEAccessibilityService extends android.accessibilityservice.AccessibilityService {
+
+    //private static final String SERVICE_ID = "sk.henrichg.phoneprofilesplusextender/.PPPEAccessibilityService";
+
+    private static final String ACTION_FOREGROUND_APPLICATION_CHANGED = "sk.henrichg.phoneprofilesplusextender.ACTION_FOREGROUND_APPLICATION_CHANGED";
+    private static final String ACTION_ACCESSIBILITY_SERVICE_UNBIND = "sk.henrichg.phoneprofilesplusextender.ACTION_ACCESSIBILITY_SERVICE_UNBIND";
+    private static final String ACCESSIBILITY_SERVICE_PERMISSION = "sk.henrichg.phoneprofilesplusextender.ACCESSIBILITY_SERVICE_PERMISSION";
+
+    private static final String EXTRA_PACKAGE_NAME = "sk.henrichg.phoneprofilesplus.package_name";
+    private static final String EXTRA_CLASS_NAME = "sk.henrichg.phoneprofilesplus.class_name";
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+
+        //Configure these here for compatibility with API 13 and below.
+        AccessibilityServiceInfo config = new AccessibilityServiceInfo();
+        config.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+
+        //Just in case this helps
+        config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+
+        setServiceInfo(config);
+    }
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        final Context context = getApplicationContext();
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+
+            try {
+                ComponentName componentName = new ComponentName(
+                        event.getPackageName().toString(),
+                        event.getClassName().toString()
+                );
+
+                ActivityInfo activityInfo = tryGetActivity(componentName);
+                boolean isActivity = activityInfo != null;
+                if (isActivity) {
+                    //Log.d("PPPEAccessibilityService", "currentActivity="+componentName.flattenToShortString());
+
+                    Intent intent = new Intent(ACTION_FOREGROUND_APPLICATION_CHANGED);
+                    intent.putExtra(EXTRA_PACKAGE_NAME, event.getPackageName().toString());
+                    intent.putExtra(EXTRA_CLASS_NAME, event.getClassName().toString());
+                    sendBroadcast(intent, ACCESSIBILITY_SERVICE_PERMISSION);
+                }
+            } catch (Exception e) {
+                Log.e("PPPEAccessibilityService.onAccessibilityEvent", e.toString());
+            }
+        }
+    }
+
+    private ActivityInfo tryGetActivity(ComponentName componentName) {
+        try {
+            return getPackageManager().getActivityInfo(componentName, 0);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        //Log.d("PPPEAccessibilityService", "onUnbind");
+
+        final Context context = getApplicationContext();
+
+        Intent _intent = new Intent(ACTION_ACCESSIBILITY_SERVICE_UNBIND);
+        sendBroadcast(_intent, ACCESSIBILITY_SERVICE_PERMISSION);
+
+        return super.onUnbind(intent);
+    }
+
+}
