9082a6db7a88261caedb91f6eb1d6a38e256aead
henrichg <henrich.gron@chello.sk>
2017-11-13 17:38:23 +0100

    Added version and list of extends to activity.

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
@@ -1,13 +1,26 @@
 package sk.henrichg.phoneprofilesplusextender;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.activity_main_application_version);
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            text.setText(getString(R.string.about_application_version) + " " + pInfo.versionName + " (" + pInfo.versionCode + ")");
+        } catch (Exception e) {
+            text.setText("");
+        }
+
     }
 }
