e5ca38f7d1254e67a564233f65b9902aff28f6ce
henrichg <henrich.gron@chello.sk>
2017-11-13 18:51:25 +0100

    Added enable/disable function of accessibility service.

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
@@ -1,12 +1,30 @@
 package sk.henrichg.phoneprofilesplusextender;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.UnderlineSpan;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_ACCESSIBILITY_SETTINGS = 1900;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -22,5 +40,77 @@ public class MainActivity extends AppCompatActivity {
             text.setText("");
         }
 
+        String str1 = getString(R.string.exend_accessibility_service);
+        String str2;
+        if (PPPEAccessibilityService.isEnabled(getApplicationContext()))
+            str2 = str1 + " " + getString(R.string.exend_accessibility_service_enabled);
+        else
+            str2 = str1 + " " + getString(R.string.exend_accessibility_service_disabled);
+        Spannable sbt = new SpannableString(str2);
+        text = findViewById(R.id.activity_main_exend_accessibility_service);
+        final Activity activity = this;
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                if (MainActivity.activityActionExists(Settings.ACTION_ACCESSIBILITY_SETTINGS, activity)) {
+                    Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                    startActivityForResult(intent, RESULT_ACCESSIBILITY_SETTINGS);
+                } else {
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                    dialogBuilder.setMessage(R.string.setting_screen_not_found_alert);
+                    //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
+                    dialogBuilder.setPositiveButton(android.R.string.ok, null);
+                    dialogBuilder.show();
+                }
+            }
+        };
+        sbt.setSpan(clickableSpan, str1.length()+1, str2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sbt.setSpan(new UnderlineSpan(), str1.length()+1, str2.length(), 0);
+        text.setText(sbt);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_ACCESSIBILITY_SETTINGS)
+            reloadActivity(this, true);
+    }
+
+    private static boolean activityActionExists(String action, Context context) {
+        try {
+            final Intent intent = new Intent(action);
+            List<ResolveInfo> activities = context.getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
+            return activities.size() > 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static void reloadActivity(Activity activity, boolean newIntent)
+    {
+        if (newIntent)
+        {
+            final Activity _activity = activity;
+            new Handler(activity.getMainLooper()).post(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        Intent intent = _activity.getIntent();
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        _activity.finish();
+                        _activity.overridePendingTransition(0, 0);
+
+                        _activity.startActivity(intent);
+                        _activity.overridePendingTransition(0, 0);
+                    } catch (Exception ignored) {}
+                }
+            });
+        }
+        else
+            activity.recreate();
+    }
+
 }
