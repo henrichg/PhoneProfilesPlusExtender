366120a7dbab3d73dccb65b941196b89f22d62e6
henrichg <henrich.gron@chello.sk>
2018-04-18 17:37:35 +0200

    Added support for force close application. (12)

---
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/ForceStopActivity.java
@@ -0,0 +1,72 @@
+package sk.henrichg.phoneprofilesplusextender;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ForceStopActivity extends AppCompatActivity {
+    static ForceStopActivity instance = null;
+    boolean appInfoClosed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    protected  void onStart() {
+        super.onStart();
+        instance = this;
+        //lockScreenOrientation();
+    }
+
+    /*
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unlockScreenOrientation();
+    }
+    */
+
+    @SuppressLint("LongLogTag")
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            Log.e("ForceStopActivity.onActivityResult", "resultCode="+resultCode);
+            appInfoClosed = true;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        instance = null;
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(0, 0);
+    }
+
+    /*
+    void lockScreenOrientation() {
+        int currentOrientation = getResources().getConfiguration().orientation;
+        if (currentOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        }
+    }
+
+    void unlockScreenOrientation() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+    }
+    */
+
+}
