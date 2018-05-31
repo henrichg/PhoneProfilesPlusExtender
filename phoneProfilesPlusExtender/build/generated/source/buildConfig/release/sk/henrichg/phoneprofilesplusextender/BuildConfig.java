9e29df622d7be827b5854db38df73681ceb4934d
henrichg <henrich.gron@chello.sk>
2018-05-31 10:37:10 +0200

    Fixed warnings. (2)

---
+++ phoneProfilesPlusExtender/build/generated/source/buildConfig/release/sk/henrichg/phoneprofilesplusextender/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sk.henrichg.phoneprofilesplusextender;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "sk.henrichg.phoneprofilesplusextender";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 72;
+  public static final String VERSION_NAME = "2.0b";
+}
