6201f285713425bd61d89b7ac6ec7a46a6d682cd
henrichg <henrich.gron@gmail.com>
2022-06-18 16:50:04 +0200

    Added support for Notifications permission (Android 13). (4)

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/MainActivity.java
@@ -115,63 +115,7 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onStart();
 
-        if (Build.VERSION.SDK_INT >= 33) {
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            if (notificationManager != null) {
-                if (!notificationManager.areNotificationsEnabled()) {
-
-                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-                    dialogBuilder.setMessage(R.string.extender_notifications_permission_text);
-                    //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
-                    dialogBuilder.setPositiveButton(R.string.extender_enable_notificaitons_button, (dialog, which) -> {
-
-                        boolean ok = false;
-
-                        Intent intent = new Intent();
-                        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-                        intent.putExtra(Settings.EXTRA_APP_PACKAGE, PPPEApplication.PACKAGE_NAME);
-
-                        if (activityIntentExists(intent, getApplicationContext())) {
-                            try {
-                                startActivity(intent);
-                                ok = true;
-                            } catch (Exception e) {
-                                PPPEApplication.recordException(e);
-                            }
-                        }
-                        if (!ok) {
-                            AlertDialog.Builder dialogBuilder1 = new AlertDialog.Builder(MainActivity.this);
-                            dialogBuilder1.setMessage(R.string.extender_setting_screen_not_found_alert);
-                            //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
-                            dialogBuilder1.setPositiveButton(android.R.string.ok, null);
-                            AlertDialog _dialog = dialogBuilder1.create();
-
-//                                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-//                                    @Override
-//                                    public void onShow(DialogInterface dialog) {
-//                                        Button positive = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
-//                                        if (positive != null) positive.setAllCaps(false);
-//                                        Button negative = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_NEGATIVE);
-//                                        if (negative != null) negative.setAllCaps(false);
-//                                    }
-//                                });
-
-                            if (!isFinishing())
-                                _dialog.show();
-                        }
-
-                    });
-                    //dialogBuilder.setNegativeButton(R.string.extender_dont_enable_notificaitons_button, null);
-
-                    AlertDialog dialog = dialogBuilder.create();
-                    dialog.setCancelable(false);
-                    dialog.setCanceledOnTouchOutside(false);
-
-                    if (!isFinishing())
-                        dialog.show();
-                }
-            }
-        }
+        Permissions.grantNotificationsPermission(this);
     }
 
     @Override
@@ -509,7 +453,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static boolean activityIntentExists(Intent intent, Context context) {
+    static boolean activityIntentExists(Intent intent, Context context) {
         try {
             List<ResolveInfo> activities = context.getApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
             return activities.size() > 0;
--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/Permissions.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/Permissions.java
@@ -2,8 +2,14 @@ package sk.henrichg.phoneprofilesplusextender;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -60,6 +66,66 @@ class Permissions {
 
             ActivityCompat.requestPermissions(activity, permArray, PERMISSIONS_REQUEST_CODE);
         //}
+    }
+
+    static void grantNotificationsPermission(final Activity activity) {
+        if (Build.VERSION.SDK_INT >= 33) {
+            NotificationManager notificationManager = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                if (!notificationManager.areNotificationsEnabled()) {
+
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                    dialogBuilder.setMessage(R.string.extender_notifications_permission_text);
+                    //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
+                    dialogBuilder.setPositiveButton(R.string.extender_enable_notificaitons_button, (dialog, which) -> {
+
+                        boolean ok = false;
+
+                        Intent intent = new Intent();
+                        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                        intent.putExtra(Settings.EXTRA_APP_PACKAGE, PPPEApplication.PACKAGE_NAME);
+
+                        if (MainActivity.activityIntentExists(intent, activity.getApplicationContext())) {
+                            try {
+                                activity.startActivity(intent);
+                                ok = true;
+                            } catch (Exception e) {
+                                PPPEApplication.recordException(e);
+                            }
+                        }
+                        if (!ok) {
+                            AlertDialog.Builder dialogBuilder1 = new AlertDialog.Builder(activity);
+                            dialogBuilder1.setMessage(R.string.extender_setting_screen_not_found_alert);
+                            //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
+                            dialogBuilder1.setPositiveButton(android.R.string.ok, null);
+                            AlertDialog _dialog = dialogBuilder1.create();
+
+//                                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+//                                    @Override
+//                                    public void onShow(DialogInterface dialog) {
+//                                        Button positive = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+//                                        if (positive != null) positive.setAllCaps(false);
+//                                        Button negative = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_NEGATIVE);
+//                                        if (negative != null) negative.setAllCaps(false);
+//                                    }
+//                                });
+
+                            if (!activity.isFinishing())
+                                _dialog.show();
+                        }
+
+                    });
+                    //dialogBuilder.setNegativeButton(R.string.extender_dont_enable_notificaitons_button, null);
+
+                    AlertDialog dialog = dialogBuilder.create();
+                    dialog.setCancelable(false);
+                    dialog.setCanceledOnTouchOutside(false);
+
+                    if (!activity.isFinishing())
+                        dialog.show();
+                }
+            }
+        }
     }
 
 }
