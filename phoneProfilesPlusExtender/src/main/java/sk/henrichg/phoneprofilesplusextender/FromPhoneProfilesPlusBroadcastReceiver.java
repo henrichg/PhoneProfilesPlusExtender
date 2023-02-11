43aeaddb56a53b24c643a0b2527e5ff60205b068
henrichg <henrich.gron@gmail.com>
2023-02-11 18:37:57 +0100

    Change: Increased contrast. (5)

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/FromPhoneProfilesPlusBroadcastReceiver.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/FromPhoneProfilesPlusBroadcastReceiver.java
@@ -141,7 +141,7 @@ class FromPhoneProfilesPlusBroadcastReceiver extends BroadcastReceiver {
         String nText = text;
         PPPEApplication.createGrantPermissionNotificationChannel(context);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, PPPEApplication.GRANT_PERMISSION_NOTIFICATION_CHANNEL)
-                .setColor(ContextCompat.getColor(context, R.color.notificationDecorationColor))
+                .setColor(ContextCompat.getColor(context, R.color.accent))
                 .setSmallIcon(R.drawable.ic_exclamation_notify) // notification icon
                 .setContentTitle(nTitle) // title for notification
                 .setContentText(nText)
