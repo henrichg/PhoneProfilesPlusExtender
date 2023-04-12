26884537437b1489830c31c6643f387f11e1b540
henrichg <henrich.gron@gmail.com>
2023-04-12 15:16:28 +0200

    Change: Added notification color into ACRA notification configuration.

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEApplication.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEApplication.java
@@ -16,6 +16,7 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.pm.PackageInfoCompat;
 
 import org.acra.ACRA;
@@ -338,6 +339,7 @@ public class PPPEApplication extends Application {
                         .withResSendButtonIcon(0)
                         .withResDiscardButtonIcon(0)
                         .withSendOnClick(true)
+                        .withColor(ContextCompat.getColor(base, R.color.notification_color))
                         .withEnabled(true)
                         .build(),
                 new MailSenderConfigurationBuilder()
