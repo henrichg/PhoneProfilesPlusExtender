4f1e34a72ad03532a77839a41521ed0b7089357a
henrichg <henrich.gron@gmail.com>
2023-04-17 17:52:13 +0200

    Updated translations.

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEApplication.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEApplication.java
@@ -16,6 +16,7 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.pm.PackageInfoCompat;
 
 import org.acra.ACRA;
@@ -338,7 +339,7 @@ public class PPPEApplication extends Application {
                         .withResSendButtonIcon(0)
                         .withResDiscardButtonIcon(0)
                         .withSendOnClick(true)
-                        //.withColor(ContextCompat.getColor(base, R.color.notification_color))
+                        .withColor(ContextCompat.getColor(base, R.color.notification_color))
                         .withEnabled(true)
                         .build(),
                 new MailSenderConfigurationBuilder()
