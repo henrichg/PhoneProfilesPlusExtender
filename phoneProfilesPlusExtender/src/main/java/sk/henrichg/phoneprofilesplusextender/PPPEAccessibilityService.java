852805e12d57d0a6bf56c05b0b6be65aa5730f15
henrichg <henrich.gron@gmail.com>
2025-09-11 14:51:45 +0200

    New Android Studio version. Updated translations. Fixed warnings.

--- phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEAccessibilityService.java
+++ phoneProfilesPlusExtender/src/main/java/sk/henrichg/phoneprofilesplusextender/PPPEAccessibilityService.java
@@ -22,6 +22,7 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import java.util.List;
 
 /** @noinspection ExtractMethodRecommender*/
+@SuppressLint("AccessibilityPolicy")
 @SuppressWarnings("SpellCheckingInspection")
 public class PPPEAccessibilityService extends android.accessibilityservice.AccessibilityService {
 
